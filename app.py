7c03cb15ebdd2475195347e6f6c5e9c113c56447
Alekya Paladugu <alekyapaladugu021@gmail.com>
2023-11-08 21:24:06 -0800

    changes

--- app.py
+++ app.py
@@ -5,28 +5,29 @@ from pydantic import ValidationError
 from tournament import Tournament
 from redis_om import Migrator
 from redis_om.model import NotFoundError
+from flask_cors import CORS, cross_origin
+import rom
 
 app = Flask(__name__)
 
+app = Flask(__name__)
+CORS(app, support_credentials=True)
 
-# Utility function to format list of People objects as 
-# a results dictionary, for easy conversion to JSON in 
-# API responses.
-def build_results(people):
-    response = []
-    for person in people:
-        response.append(person.dict())
+@app.route("/")
+@cross_origin(supports_credentials=True)
+def login():
+  return jsonify({'success': 'ok'})
 
-    return { "results": response }
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8000, debug=True)
 
 # Create a new tournament.
 @app.route("/tournament/new", methods=["POST"])
 def create_tournament():
     try:
-        print(request.json)
         new_tournament = Tournament(**request.json)
         new_tournament.save()
-        return 'Successful'
+        return jsonify({'success': True}), 200
 
     except ValidationError as e:
         print(e)
@@ -46,5 +47,27 @@ def get_tournaments():
                 tournaments_dict[key] = value.decode('utf-8')
         tournaments_list.append(tournaments_dict)
     # Return JSON response
-    print(tournaments_list)
-    return jsonify(tournaments_list)
+    return jsonify({'tournaments': tournaments_list})
+
+# Update a tournament. Use where clauses
+@app.route('/tournament/update', methods=['POST'])
+def update():
+    key = request.json['key_id']
+    host_country = request.json['host_country']
+    if not key:
+        return jsonify({'error': 'Missing key'}), 400
+    update_existing_tournament = Tournament.query.filter(key_id=key).all()
+    if update_existing_tournament is None:
+        return jsonify({'error': 'No such key/column exists'}), 404
+    else:
+        update_existing_tournament.host_won = host_country
+        update_existing_tournament.save()
+        return jsonify({'success': True}), 200
+
+@app.after_request
+def after(response):
+    # todo with response
+    print(response.status)
+    print(response.headers)
+    print(response.get_data())
+    return response
--- dataloader.py
+++ dataloader.py
@@ -1,5 +1,6 @@
 import csv
 import requests
+import json
 
 with open('data/tournaments.csv', encoding = 'utf-8') as csv_file_handler:
     reader = csv.DictReader(csv_file_handler)
@@ -9,4 +10,26 @@ for row in data:
     r = requests.post('http://127.0.0.1:5000/tournament/new', json=row)
     print(f"Created tournament {row['key_id']}")
 
-# r = requests.get('http://127.0.0.1:5000/tournaments')
+# data =  {
+#         "count_teams": 13,
+#         "end_date": "1930-07-30",
+#         "final": 1,
+#         "final_round": 0,
+#         "group_stage": 1,
+#         "host_country": "Uruguay",
+#         "host_won": 0,
+#         "id": 1,
+#         "key_id": 1,
+#         "quarter_finals": 0,
+#         "round_of_16": 0,
+#         "second_group_stage": 0,
+#         "semi_finals": 1,
+#         "start_date": "",
+#         "third_place_match": 0,
+#         "tournament_id": "WC-1930",
+#         "tournament_name": "1930 FIFA Men's World Cup",
+#         "winner": "Uruguay",
+#         "year": 1930
+#     },
+
+# r = requests.post('http://127.0.0.1:5000/tournament/update', json=data, headers={'Content-Type': 'application/json'})
